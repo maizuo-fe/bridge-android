7c3084886902bf16000dc031b108a50efdedd856
yelo <zhihuzeye@gmail.com>
2014-11-03 15:37:26 +0800

    decoupling gulp-duojs

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,9 @@
 var gulp = require('gulp');
-
-var Duo = require('duo');
-var through = require('through2');
-var _ = require('lodash');
-var gutil = require('gulp-util');
-var PluginError = gutil.PluginError;
-
-var root = __dirname;
-
-function gulpDuo (opts) {
-
-  var options = _.defaults(opts || {}, {
-    standalone: ''
-  });
-
-  function compile (path, callback) {
-    Duo(root).entry(path).standalone(options.standalone).run(function (err, bundle) {
-      callback(null, bundle);
-    });
-  }
-
-  function duo (file, encoding, callback) {
-    if (file.isNull()) {
-      callback(null, file);
-    }
-    compile(file.path, function (err, bundle) {
-      if (err) {
-        callback(err);
-      }
-      if (file.isBuffer()) {
-        file.contents = new Buffer(bundle);
-      }
-      if (file.isStream()) {
-        file.contents.write(bundle);
-      }
-      callback(null, file);
-    });
-  }
-
-  return through.obj(duo);
-}
-
+var duo = require('gulp-duojs');
 
 gulp.task('compile', function () {
   gulp.src('lib/bridge.js')
-    .pipe(gulpDuo({standalone: 'AndroidBridge'}))
+    .pipe(duo({standalone: 'AndroidBridge'}))
     .pipe(gulp.dest('./dest'));
 });
 
